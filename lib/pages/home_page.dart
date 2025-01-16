adfafe06645fdff5d45e800c8b00378d18ae967d
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-01-16 12:31:47 +0100

    done building firebase note app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -40,6 +40,64 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  void handleUpdate(String note, int index, List notesList) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        TextEditingController updateController =
+            TextEditingController(text: note);
+        return AlertDialog(
+          content: TextField(
+            controller: updateController,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                firestore.updateNote(
+                    notesList[index].id, updateController.text);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Update'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget itemBuilder(BuildContext context, List notesList, int index) {
+    String note = notesList[index]['note'];
+    return ListTile(
+      title: Text(
+        note,
+        overflow: TextOverflow.ellipsis, // Ensures long text doesn't overflow
+      ),
+      trailing: IntrinsicWidth(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () => handleUpdate(note, index, notesList),
+            ),
+            IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                firestore.deleteNote(notesList[index].id);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,6 +118,20 @@ class _HomePageState extends State<HomePage> {
           color: Colors.white,
         ),
       ),
+      body: StreamBuilder(
+          stream: firestore.getNotesStream(),
+          builder: (context, snapshop) {
+            if (snapshop.hasData) {
+              List notesList = snapshop.data!.docs;
+              return ListView.builder(
+                itemCount: notesList.length,
+                itemBuilder: (context, index) =>
+                    itemBuilder(context, notesList, index),
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          }),
     );
   }
 }
--- lib/service/firestore.dart
+++ lib/service/firestore.dart
@@ -14,4 +14,30 @@ class Firestore {
           name: 'Firestore', level: 1000); // Logging the error
     }
   }
+
+  Stream<QuerySnapshot> getNotesStream() {
+    final notesStream =
+        notes.orderBy('timestamp', descending: true).snapshots();
+    return notesStream;
+  }
+
+  Future<void> updateNote(String noteId, String newNote) async {
+    try {
+      await notes
+          .doc(noteId)
+          .update({'note': newNote, 'timestamp': Timestamp.now()});
+      log('Note updated successfully', name: 'Firestore');
+    } catch (e) {
+      log('Error updating note: $e', name: 'Firestore', level: 1000);
+    }
+  }
+
+  Future<void> deleteNote(String noteId) async {
+    try {
+      await notes.doc(noteId).delete();
+      log('Note deleted successfully', name: 'Firestore');
+    } catch (e) {
+      log('Error deleting note: $e', name: 'Firestore', level: 1000);
+    }
+  }
 }
